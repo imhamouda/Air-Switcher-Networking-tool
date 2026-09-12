cb49c7cbad47329a3f982fb4c2eb62365a4d3602
imhamouda <eyadhamouda2009@gmail.com>
2026-09-12 20:52:24 +0100

    Added an automatic build directory creator

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p build
 g++ -std=c++14 -shared -fPIC -o build/libsniffer-test.so src/sniffer.cpp \
     -I/usr/local/include -L/usr/local/lib -ltins -lpcap
 echo "[+] Build complete: build/libsniffer-test.so"
